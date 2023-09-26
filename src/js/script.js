f54d4a5931881125dc50b80e7554e8ec119e9b12
cross219 <powder.snow19@gmail.com>
2023-09-26 21:35:41 +0900

    修正後

--- src/js/script.js
+++ src/js/script.js
@@ -29,7 +29,6 @@ jQuery(function ($) {
       }
     });
   });
-
   //ドロワーメニュー
   $(".js-hamburger").on("click", function () {
     if ($(".js-hamburger").hasClass("is-open")) {
@@ -42,7 +41,6 @@ jQuery(function ($) {
       $("body").css("overflow", "hidden");
     }
   });
-
   // //swiper(fv)
   let swiper_works = new Swiper(".js-fv-swiper", {
     loop: true,
@@ -52,7 +50,6 @@ jQuery(function ($) {
       delay: 4000,
     },
   });
-
   var swiper = new Swiper(".js-campaign-swiper", {
     loop: true, // ループ
     spaceBetween: 24, // スライド間の距離
@@ -73,7 +70,6 @@ jQuery(function ($) {
       nextEl: ".campaign-swiper__button-next",
       prevEl: ".campaign-swiper__button-prev",
     },
-
     breakpoints: {
       768: {
         // slidesPerView: 3.5,
@@ -87,14 +83,11 @@ jQuery(function ($) {
       // },
     },
   });
-
   //  ヘッダークラス名付与
   // let header = $(".header");
   // let headerHeight = $(".header").height();
   let height = $(".fv ,.sub-mv").height();
-
   var topBtn = $(".to-top");
-
   // ボタンの表示設定
   $(window).scroll(function () {
     if ($(this).scrollTop() > height) {
@@ -116,21 +109,31 @@ jQuery(function ($) {
     );
     return false;
   });
-
-  const tabs = ".js-tab";
-  const contents = ".js-content";
-  const lists = ".js-list";
-  const activeClass = "is-active";
-
   //タブ切り替え
-  $(tabs).on("click", function () {
+  $(".js-tab").on("click", function () {
     const i = $(this).index();
-    $(tabs).removeClass(activeClass);
-    $(contents).removeClass(activeClass);
-    $(this).addClass(activeClass);
-    $(contents).eq(i).addClass(activeClass);
+    $(".js-tab").removeClass("is-active");
+    $(".js-content").removeClass("is-active");
+    $(this).addClass("is-active");
+    $(".js-content").eq(i).addClass("is-active");
   });
-
+  //info tab
+  $(function () {
+    $(".js-info-tab").on("click", function () {
+      $(".current").removeClass("current");
+      $(this).addClass("current");
+      var index = $(this).index();
+      $(".js-info-content").hide().eq(index).fadeIn(300);
+    });
+    var urlParams = new URLSearchParams(window.location.search);
+    var tabId = urlParams.get("tabId");
+    if (tabId) {
+      $(".current").removeClass("current");
+      $('.js-info-tab[data-tab-id="'.concat(tabId, '"]')).addClass("current");
+      var index = $('.js-info-tab[data-tab-id="'.concat(tabId, '"]')).index();
+      $(".js-info-content").hide().eq(index).fadeIn(100);
+    }
+  });
   //モーダル
   $(function () {
     // モーダルを表示する関数
@@ -138,72 +141,53 @@ jQuery(function ($) {
       $(".gallery__modal-image").attr("src", imageUrl);
       $(".gallery__modal").addClass("active");
     }
-
     // モーダルを非表示にする関数
     function hideModal() {
       $(".gallery__modal").removeClass("active");
     }
-
     // 画像をクリックしたらモーダルを表示
     $(".gallery__item.js-modal").on("click", function () {
       var imageUrl = $(this).find("img").attr("src");
       showModal(imageUrl);
     });
-
     // モーダルをクリックして閉じる
     $(".gallery__modal").on("click", function () {
       hideModal();
     });
-
     // モーダル外の領域をクリックしたらモーダルを非表示にする
     $(".gallery__modal-overlay").on("click", function () {
       hideModal();
     });
   });
-
-
-$(document).ready(function () {
-  $(".faq__content").slideDown(); // すべてのアコーディオンを開く
-  $(".faq__icon").addClass("is-open"); // すべてのアイコンを開いた状態にする
-});
-
-$(".js-faq").on("click", function () {
-  // クリックされたアコーディオン要素を取得
-  var clickedAccordion = $(this);
-
-  // クリックされたアコーディオン以外を閉じる
-  // $(".faq__content").not(clickedAccordion.next(".faq__content")).slideUp(); // 他のアコーディオンを閉じる
-
-  // クリックされたアコーディオンを開閉する
-  clickedAccordion.find(".faq__icon").toggleClass("is-open");
-  var findElm = clickedAccordion.next(".faq__content");
-  findElm.slideToggle();
-
-  if (clickedAccordion.hasClass("close")) {
-    clickedAccordion.removeClass("close");
-  } else {
-    clickedAccordion.addClass("close");
-  }
-});
-
-
-  
-
-
+  //アコーディオンメニュー
+  $(document).ready(function () {
+    $(".faq__content").slideDown(); // すべてのアコーディオンを開く
+    $(".faq__icon").addClass("is-open"); // すべてのアイコンを開いた状態にする
+  });
+  $(".js-faq").on("click", function () {
+    // クリックされたアコーディオン要素を取得
+    var clickedAccordion = $(this);
+    // クリックされたアコーディオンを開閉する
+    clickedAccordion.find(".faq__icon").toggleClass("is-open");
+    var findElm = clickedAccordion.next(".faq__content");
+    findElm.slideToggle();
+    if (clickedAccordion.hasClass("close")) {
+      clickedAccordion.removeClass("close");
+    } else {
+      clickedAccordion.addClass("close");
+    }
+  });
+  //form
   $(document).ready(function () {
     // 必須項目の入力要素を取得
     const requiredInputs = $(".required");
-  
     // エラーメッセージを表示する要素を取得
     const errorElement = $(".js-error");
-  
     // 送信ボタンを取得
     const submitButton = $("#submit-button");
-  
     // フォーム送信時の処理を設定
     $("#form").on("submit", function (e) {
       let hasError = false;
-  
       // 必須項目の入力が空かどうかをチェック
       requiredInputs.each(function () {
         if ($(this).val().trim() === "") {
@@ -214,11 +198,9 @@ $(".js-faq").on("click", function () {
           $(this).removeClass("error");
         }
       });
-  
       // エラーがある場合はフォーム送信をキャンセル
       if (hasError) {
         e.preventDefault();
-  
         // エラーがある場合、フォームの上部までスクロール
         $("body,html").animate(
           {
@@ -229,7 +211,6 @@ $(".js-faq").on("click", function () {
         );
       }
     });
-  
     // 入力要素にフォーカスがあたったときの処理を設定
     requiredInputs.on("blur", function () {
       // 未入力の場合、.error クラスを追加
@@ -242,7 +223,6 @@ $(".js-faq").on("click", function () {
         errorElement.css("display", "none"); // エラーメッセージを非表示
       }
     });
-  
     // .js-submit-checkの変更時に送信ボタンのdisabled属性を制御
     $(".js-submit-check").on("change", function () {
       if ($(this).is(":checked")) {
@@ -252,7 +232,4 @@ $(".js-faq").on("click", function () {
       }
     });
   });
-
-
 });
-
